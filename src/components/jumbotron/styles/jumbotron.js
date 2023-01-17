c3a166f2aaff0e76c2bc5e8c44079ab838cf331f
Cameron <sicklecj@gmail.com>
2023-01-17 14:51:14 -0600

    move container to seperate file location and link

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -23,34 +23,40 @@ export const Inner = styled.div`
 `;
 
 export const Pane = styled.div`
-width: 50%;
+    width: 50%;
 
-@media (max-width: 1000px){
-    width:100%;
-    padding: 0 45px;
-    text-align: center;
+    @media (max-width: 1000px){
+        width:100%;
+        padding: 0 45px;
+        text-align: center;
 }
 `;
 
 export const Title = styled.h1`
-font-size: 50px;
-line-height 1.1;
-margin-bottom: 8px;
+    font-size: 50px;
+    line-height 1.1;
+    margin-bottom: 8px;
 
-@media (max-width: 600px){
-    font-size:35px;
+    @media (max-width: 600px){
+        font-size:35px;
 }
 `;
 
 export const SubTitle = styled.h2`
-font-size: 26px;
-font-weight: normal;
-line-height: normal;
+    font-size: 26px;
+    font-weight: normal;
+    line-height: normal;
 `;
 
 export const Image = styled.img`
-max-width: 100%;
-height: auto;
+    max-width: 100%;
+    height: auto;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    @media (max-width: 1000px) {
+        ${Item}: last-of-type h2 {
+            margin-bottom: 50px;
+        }
+    }
+`;
