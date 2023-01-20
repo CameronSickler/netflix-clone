fc26020b9de49bdc9821a54a9dba64bdea1dc454
Cameron <sicklecj@gmail.com>
2023-01-20 15:23:51 -0600

    remove frame from accordion

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -15,24 +15,62 @@ export const Inner = styled.div`
 
 export const Item = styled.div`
     color: white;
+    margin: auto;
     margin-bottom: 10px;
-
+    max-width: 728px;
+    width: 100%;
+    
     &:first-of-type {
         margin-top: 3em;
+    }
+    &:last-of-type {
+        margin-bottom: 0;
     }
 `;
 
 export const Header = styled.div`
     display: flex;
     justify-content: space-between;
+    cursor: pointer;
+    margin-bottom: 1px;
+    font-size: 26px;
+    font-weight: normal;
+    background: #303030;
+    padding: 0.8 1.2em;
+    user-select: none;
+    align-items: center;
+
+    img {
+        filter: brightness(0) invert(1);
+        width: 24px;
+        user-select: none;
+
+        @media (max-width: 600px) {
+            width: 16px;
+        }
+    }
+
+    @meida (max-width: 600px) {
+        font-size: 16px;
+    }
 `;
 
 export const Body = styled.div`
+    max-height: 1200px;
+    transition: max-height 0.25s cubic-bezier(0.5, 0, 0.1, 1);
+    font-size: 26px;
+    font-weight: normal;
+    line-height: normal;
+    background: #303030;
+    padding: 0.8em 2.2em 0.8em 1.2em;
+    white-space: pre-wrap;
+    user-select: none;
 
-`;
+    @media (max-width: 600px) {
+        font-size: 16px;
+        line-height: 22px;
 
-export const Frame = styled.div`
-    margin-bottom: 40px;
+    }
 `;
 
 export const Title = styled.h1`
