d3868bcb98c6a4e15538336892d5da11ba0a7249
Cameron <sicklecj@gmail.com>
2023-01-20 15:01:34 -0600

    add close toggle

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,16 +1,50 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    border-bottom: 8px solid #222;
+`;
 
-export const Item = styled.div``;
+export const Inner = styled.div`
+    display: flex;
+    padding: 70px 45px;
+    flex-direction: column;
+    max-width: 815px;
+    margin: auto;    
+`;
 
-export const Header = styled.div``;
+export const Item = styled.div`
+    color: white;
+    margin-bottom: 10px;
 
-export const Body = styled.div``;
+    &:first-of-type {
+        margin-top: 3em;
+    }
+`;
 
-export const Frame = styled.div``;
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
 
-export const Inner = styled.div``;
+export const Body = styled.div`
 
-export const Title = styled.h1``;
+`;
+
+export const Frame = styled.div`
+    margin-bottom: 40px;
+`;
+
+export const Title = styled.h1`
+    font-size: 50px;
+    line-height: 1.1;
+    margin-top: 0;
+    margin-bottom: 8px;
+    color: white;
+    text-align: center;
+
+    @media (max-width: 600px) {
+        font-size: 35px;
+    }
+`;
 
