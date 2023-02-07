55533344757f3a9363befd9af47eb6d96917d91d
Cameron <sicklecj@gmail.com>
2023-02-06 18:16:30 -0600

    add firebase but has index.js bug not importing firebase

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,12 +1,20 @@
-import Firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
-//we need to seed the database
+const config = {
+    apiKey: "AIzaSyCcaSY9o4Nwupf0Pfse-HhwQm4HjhgJzDE",
+    authDomain: "netflix-clone-38a3f.firebaseapp.com",
+    projectId: "netflix-clone-38a3f",
+    storageBucket: "netflix-clone-38a3f.appspot.com",
+    messagingSenderId: "1047486154470",
+    appId: "1:1047486154470:web:89ede848250d4eb2a0dd27",
+};
 
-//we need a config here
-const config = {};
+const firebase = initializeApp(config);
 
-const firebase = Firebase.initializeApp(config);
+seedDatabase(firebase);
 
 export { firebase };
+
