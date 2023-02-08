ded1c51127e197e1d41cba06863fe773c1fc1044
Cameron <sicklecj@gmail.com>
2023-02-08 12:54:30 -0600

    debugging firebase

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { } from 'firebase/firestore';
+import { } from 'firebase/auth';
 import { seedDatabase } from '../seed';
 
-const config = {
+const firebaseConfig = {
     apiKey: "AIzaSyCcaSY9o4Nwupf0Pfse-HhwQm4HjhgJzDE",
     authDomain: "netflix-clone-38a3f.firebaseapp.com",
     projectId: "netflix-clone-38a3f",
@@ -12,9 +12,10 @@ const config = {
     appId: "1:1047486154470:web:89ede848250d4eb2a0dd27",
 };
 
-const firebase = initializeApp(config);
+const firebase = initializeApp(firebaseConfig);
 
 seedDatabase(firebase);
 
 export { firebase };
+
 
