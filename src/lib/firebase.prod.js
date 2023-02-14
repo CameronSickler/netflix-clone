547f4f174b838afdc827e2f82833624b312e7d16
Cameron <sicklecj@gmail.com>
2023-02-14 16:22:58 -0600

    remove API

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,12 +4,7 @@ import 'firebase/auth';
 import { seedDatabase } from '../seed';
 
 const config = {
-    apiKey: "AIzaSyCcaSY9o4Nwupf0Pfse-HhwQm4HjhgJzDE",
-    authDomain: "netflix-clone-38a3f.firebaseapp.com",
-    projectId: "netflix-clone-38a3f",
-    storageBucket: "netflix-clone-38a3f.appspot.com",
-    messagingSenderId: "1047486154470",
-    appId: "1:1047486154470:web:89ede848250d4eb2a0dd27",
+    //    API here
 };
 
 const firebase = initializeApp(config);
